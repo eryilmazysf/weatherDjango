71bbc123193c99642f8cf75b6f53bb546f8c33e5
Yusuf Eryilmaz <yusuferyilmaz1819@gmail.com>
2021-01-08 15:37:21 -0500

    weatherApp

---
+++ weatherProject/weatherApp/views.py
@@ -0,0 +1,27 @@
+from django.shortcuts import render
+from decouple import config
+
+import urllib.request
+import json
+
+
+def index(request):
+    if request.method == "POST":
+        city = request.POST['city']
+        api_key = config('API_KEY')
+        source = urllib.request.urlopen(
+            'http://api.openweathermap.org/data/2.5/weather?q='+city+'&units=metric&appid='+api_key).read()
+        list_of_data = json.loads(source)
+        data = {
+            "country_code": str(list_of_data['sys']['country']),
+            "temp": str(list_of_data['main']['temp'])+'°C',
+            "pressure": str(list_of_data['main']['pressure']),
+            "humidity": str(list_of_data['main']['humidity']),
+            "main": str(list_of_data['weather'][0]['main']),
+            "description": str(list_of_data['weather'][0]['description']),
+            "icon": list_of_data['weather'][0]['icon'],
+        }
+        print(data)
+    else:
+        data = {}
+    return render(request, 'main/index.html', data)
